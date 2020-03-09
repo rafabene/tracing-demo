14afe2568b7643c566cc095d28250d1f3a28e17e
Rafael Benevides <rafabene@gmail.com>
2020-03-09 14:13:35 -0300

    Update run script to show jaeger port

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export MS_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 
-url="http://192.168.100.100:$INGRESS_PORT"
+url="http://192.168.100.100:$MS_INGRESS_PORT"
 echo "Ingress URLS: " 
 echo "SERIAL:" $url/serial/Rafael
 echo "PARALLEL:" $url/parallel/Rafael
 echo "Chain:" $url/chain/Rafael
+
+
+kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 15032:16686 &
+sleep 1
+DASHBOARD="http://localhost:15032/jaeger/search"
+echo "JAEGER:" $DASHBOARD
