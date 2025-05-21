64949bef34ca36978bb5d6213f98b4e6824b55a0
Rafael Benevides <rafabene@gmail.com>
2025-05-21 19:08:30 -0300

    Removed Jaeger

--- open-jaeger.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 15032:16686 &
-sleep 1
-DASHBOARD="http://localhost:15032/jaeger/search"
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    gio open $DASHBOARD 
-elif [[ "$OSTYPE" == "darwin"* ]]; then 
-    open $DASHBOARD
-fi
